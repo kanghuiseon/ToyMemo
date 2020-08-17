aba03813b9a1dde4794e1bf6c34d9866d0baedc8
2016025123 <ekfsla0113@hanyang.ac.kr>
2020-08-17 22:04:26 +0900

    set date using date formatter

--- ToyMemo/MemoListTableViewController.swift
+++ ToyMemo/MemoListTableViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class MemoListTableViewController: UITableViewController {
-
+    
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -28,7 +36,7 @@ class MemoListTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let target = Memo.dummyMemoList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate.description
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
         return cell
     }
     
