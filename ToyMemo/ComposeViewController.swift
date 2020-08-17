87a23213b69464567f835552bc933182bfa23ecc
2016025123 <ekfsla0113@hanyang.ac.kr>
2020-08-17 22:26:25 +0900

    implement plus button from main view.

---
+++ ToyMemo/ComposeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ComposeViewController.swift
+//  ToyMemo
+//
+//  Created by 강희선 on 2020/08/17.
+//  Copyright © 2020 강희선. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
